7f60f4a458756a3cc7f9cbe68ea8cc83b46ab46e
Choi-Big-Root <bigroot0601@gmail.com>
2025-07-16 09:01:23 +0900

    customTextTheme 내부에서 letterSpacing 지정..

--- lib/core/custom/custom_theme.dart
+++ lib/core/custom/custom_theme.dart
@@ -6,65 +6,80 @@ import 'custom_font_weight.dart';
 TextTheme customTextTheme(ThemeColors colors) {
   return TextTheme(
     displayLarge: TextStyle(
+      letterSpacing: 0.0,
       color: colors.primaryText,
       fontSize: 57,
       fontWeight: CustomFontWeight.normal,
     ),
     displayMedium: TextStyle(
+        letterSpacing: 0.0,
         color: colors.primaryText,
         fontSize: 45,
         fontWeight: CustomFontWeight.normal),
     displaySmall: TextStyle(
+      letterSpacing: 0.0,
       color: colors.primaryText,
       fontSize: 24,
       fontWeight: CustomFontWeight.semiBold,
     ),
     headlineLarge: TextStyle(
+        letterSpacing: 0.0,
         color: colors.primaryText,
         fontSize: 32,
         fontWeight: CustomFontWeight.normal),
     headlineMedium: TextStyle(
+        letterSpacing: 0.0,
         color: colors.primaryText,
         fontSize: 22,
         fontWeight: CustomFontWeight.medium),
     headlineSmall: TextStyle(
+        letterSpacing: 0.0,
         color: colors.primaryText,
         fontSize: 20,
         fontWeight: CustomFontWeight.bold),
     titleLarge: TextStyle(
+        letterSpacing: 0.0,
         color: colors.primaryText,
         fontSize: 22,
         fontWeight: CustomFontWeight.medium),
     titleMedium: TextStyle(
+        letterSpacing: 0.0,
         color: colors.secondaryText,
         fontSize: 18,
         fontWeight: CustomFontWeight.medium),
     titleSmall: TextStyle(
+        letterSpacing: 0.0,
         color: colors.primaryText,
         fontSize: 16,
         fontWeight: CustomFontWeight.semiBold),
     labelLarge: TextStyle(
+        letterSpacing: 0.0,
         color: colors.primaryText,
         fontSize: 14,
         fontWeight: CustomFontWeight.medium),
     labelMedium: TextStyle(
+        letterSpacing: 0.0,
         color: colors.primaryText,
         fontSize: 12,
         fontWeight: CustomFontWeight.medium),
     labelSmall: TextStyle(
+        letterSpacing: 0.0,
         color: colors.primaryText,
         fontSize: 11,
         fontWeight: CustomFontWeight.medium),
     bodyLarge: TextStyle(
+      letterSpacing: 0.0,
       color: colors.primaryText,
       fontSize: 16,
       fontWeight: CustomFontWeight.normal,
     ),
     bodyMedium: TextStyle(
+        letterSpacing: 0.0,
         color: colors.secondaryText,
         fontSize: 14,
         fontWeight: CustomFontWeight.medium),
     bodySmall: TextStyle(
+        letterSpacing: 0.0,
         color: colors.primaryText,
         fontSize: 14,
         fontWeight: CustomFontWeight.medium),
